c418d4926110263c99ed1be73340cfaaa9b59faa
rosz <anna.roszman@gmail.com>
2017-09-27 20:51:18 +0200

    delete redundant code, delete unnecessary 2nd argument in methods, set variables in methods for previous method result

--- scraper.py
+++ scraper.py
@@ -8,9 +8,8 @@ class ArtistsScraper():
     link = 'https://artmuseum.pl/pl/kolekcja/artysci'
 
     # get list of links to artists's individual pages
-    def get_artists_list(self, link):
+    def get_artists_list(self):
         artists_list = requests.get(self.link, verify=False)
-        print(artists_list)
         text = artists_list.text
         soup = BeautifulSoup(text, 'html.parser')
         artists_ul = soup.find_all('ul', class_='artist-list')[0]
@@ -18,11 +17,10 @@ class ArtistsScraper():
 
         links_list = [link['href'] for link in hrefs_list]
         urls_list = [ArtistsScraper.main_link + element for element in links_list]
-        print(urls_list)
         return urls_list
 
     # get list of artists' ages
-    def get_ages(self, urls_list):
+    def get_ages(self):
         urls_list = self.get_artists_list()
         ages = []
 
@@ -38,17 +36,10 @@ class ArtistsScraper():
         return ages
 
     # get average of ages
-    def get_average(self, ages):
+    def get_average(self):
         ages = self.get_ages()
         average = 0
         for age in ages:
             average += age
         result = average / len(ages)
         return result
-
-if __name__ == '__main__':
-    current_scraper = ArtistsScraper()
-    print(current_scraper)
-
-
-# print(ArtistsScraper.get_average(ArtistsScraper.get_ages(ArtistsScraper.get_artists_list("https://artmuseum.pl/pl/kolekcja/artysci"))))
