79ac8121ff936f2c4312d4420da3c6efc926b76b
rosz <anna.roszman@gmail.com>
2017-09-27 21:10:14 +0200

    run script using three methods, providing previous methods' results as args

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from scraper import ArtistsScraper
 
 if __name__ == '__main__':
     current_scraper = ArtistsScraper()
-    print(current_scraper.get_average())
+    a = current_scraper.get_artists_list()
+    b = current_scraper.get_ages(a)
+    c = current_scraper.get_average(b)
+    print(c)
